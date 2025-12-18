bd98cd635b2c4170be17eda47e98e4481aaede85
Alozialo <ion.olaizola@gmail.com>
2025-12-18 20:45:08 +0100

    Commit despues de tener repositorio remoto

--- probandogithub.py
+++ probandogithub.py
@@ -3,3 +3,5 @@ print("Primer print")
 print("Segundo print")
 
 print("Cuarto print en total, pero tercero si no tenemos en cuenta el de Alternative")
+
+print("Siguiente print despues de suber el anterior commit a github")
