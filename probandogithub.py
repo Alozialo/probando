608664b57a7891e4f4b04f57fc5d274dab988f04
Alozialo <ion.olaizola@gmail.com>
2025-12-24 16:38:59 +0100

    Añado texto de dirección como nota

--- probandogithub.py
+++ probandogithub.py
@@ -9,3 +9,14 @@ print("Siguiente print después de subir el anterior commit a github")
 print("Corrigo ortografía en el print de la línea 7 y añado este print")
 
 print("Otro print más para probar el push sin cambios en el remoto")
+
+
+'''
+PARA GITBASH
+
+cd Python/1_PRUEBAS/P008_PROBANDO_GITHUB
+
+
+
+
+'''
